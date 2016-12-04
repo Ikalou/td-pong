95d45279d3637ece37c2b188530f694165ad9ea6
Vianney Petit <vianneypetit@gmail.com>
2016-12-05 00:16:00 +0100

    Delete test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-TextModifierBehavior=function(_super){function TextModifierBehavior(){_super.apply(this,arguments),this.pixelated=!0}return __extends(TextModifierBehavior,_super),TextModifierBehavior.prototype.awake=function(){var texture=this.actor.textRenderer.__inner.texture;this.pixelated===!0?(texture.magFilter=THREE.NearestFilter,texture.minFilter=THREE.NearestFilter):this.pixelated===!1&&(texture.magFilter=THREE.LinearFilter,texture.minFilter=THREE.LinearFilter)},TextModifierBehavior.prototype.update=function(){},TextModifierBehavior}(Sup.Behavior);Sup.registerBehavior(TextModifierBehavior);
-
-
-
-
